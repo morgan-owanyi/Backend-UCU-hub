5478af6e86a8148734194e129b66ab2dd0cd27fe
morgan owanyi <morganowanyii>
2026-03-24 22:55:25 +0300

    added requirements.txt, updated settings.py to allow all hosts, and deleted old requirements.txt

--- ucu_hub/config/settings.py
+++ ucu_hub/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-186*wc+$+&$tyx6y5-k!4*(cdcw&ynq@f-^_)lo1ji3srlftn^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
